e6a7c30bcfa00cd180e8063742a210ccecb99bff
Cowtowncoder <tatu.saloranta@iki.fi>
2015-10-15 14:24:39 -0700

    pom cleanup

--- src/main/java/com/fasterxml/classmate/types/ResolvedRecursiveType.java
+++ src/main/java/com/fasterxml/classmate/types/ResolvedRecursiveType.java
@@ -48,7 +48,6 @@ public class ResolvedRecursiveType extends ResolvedType
         }
         _referencedType = ref;
     }
-    
 
     /*
     /**********************************************************************
