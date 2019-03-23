fa9a5988afa7b8f8a4266c8e20c8dcb9ea1d4cdf
Tatu Saloranta <tatu.saloranta@iki.fi>
2019-03-22 20:03:11 -0700

    prepare for 1.5.0

---
+++ src/moditect/module-info.java
@@ -0,0 +1,7 @@
+// NOTE: auto-generated with Moditect plugin, on 22-Mar-2019
+module com.fasterxml.classmate {
+    exports com.fasterxml.classmate;
+    exports com.fasterxml.classmate.members;
+    exports com.fasterxml.classmate.types;
+    exports com.fasterxml.classmate.util;
+}
