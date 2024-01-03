74ed736f3f5dbfc5e997d6c5396e22d4780fa0e2
Tatu Saloranta <tatu.saloranta@iki.fi>
2024-01-02 16:55:22 -0800

    Fixes #51: make `ResolvedArrayType` have `java.lang.Object` as parent type

--- src/test/java/com/fasterxml/classmate/types/ResolvedArrayType51Test.java
+++ src/test/java/com/fasterxml/classmate/types/ResolvedArrayType51Test.java
@@ -1,10 +1,10 @@
-package com.fasterxml.classmate.failing;
+package com.fasterxml.classmate.types;
 
 import com.fasterxml.classmate.BaseTest;
 import com.fasterxml.classmate.ResolvedType;
 import com.fasterxml.classmate.TypeResolver;
 
-public class ArrayTypeResolution51Test extends BaseTest
+public class ResolvedArrayType51Test extends BaseTest
 {
     protected final TypeResolver RESOLVER = new TypeResolver();
 
@@ -13,6 +13,6 @@ public class ArrayTypeResolution51Test extends BaseTest
         ResolvedType rt = RESOLVER.resolve(Long[].class);
         ResolvedType parent = rt.getParentClass();
         assertNotNull(parent);
-        assertEquals(Long.class, parent.getErasedType());
+        assertEquals(Object.class, parent.getErasedType());
     }
 }
